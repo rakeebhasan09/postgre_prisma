be7daaaa3c1f983cb182e349bc2d0be220752a5a
rakeebhasan09 <mdrakeebhasan1@gmail.com>
2026-08-17 00:06:30 +0600

    Start creating service code.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
-import express from "express";
+import express, { Router } from "express";
 import cors from "cors";
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/", Router);
 
 app.get("/", (req, res) => {
     res.json({
---
+++ src/services/products.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import prisma from "../lib/prisma";
+
+const router = Router();
+
+// For creating a new product
+router.post("/products", async (req, res) => {
+    try {
+        const productData = req.body;
+        console.log("Received product data:", productData);
+        const data = await prisma.products.create(productData);
+        res.json({
+            success: true,
+            message: "Product created successfully",
+            data,
+        });
+    } catch (error) {
+        console.error("Error creating product:", error);
+        res.status(500).json({
+            success: false,
+            message: "Error creating product",
+            data: error,
+        });
+    }
+});
