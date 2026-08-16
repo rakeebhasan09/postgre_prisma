9016f27a9c2ecfb014330637e14421350bd3bc48
rakeebhasan09 <mdrakeebhasan1@gmail.com>
2026-08-16 18:08:04 +0600

    Declear Products and Orders schema, generate- generated folder. Now starting write service code.

---
+++ prisma/migrations/20260816120148_db_migrate/migration.sql
@@ -0,0 +1,39 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('User', 'Admin', 'Manager');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'User',
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "products" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "price" INTEGER NOT NULL,
+    "stock" INTEGER NOT NULL,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "orders" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "quantity" INTEGER NOT NULL,
+
+    CONSTRAINT "orders_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "orders" ADD CONSTRAINT "orders_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
-import express from "express";
-import cors from "cors";
+import dotenv from "dotenv";
+import app from "./app";
+dotenv.config();
 
-const app = express();
-app.use(cors());
+const PORT = process.env.PORT || 5000;
 
-const startServer = async () => {
-    app.listen(process.env.PORT || 3000);
-};
-startServer();
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
